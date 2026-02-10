1a5153f54028aba2736da9fcd8dda96e5c5b864e
Bryant Monahan <bryantmonahan@gmail.com>
2026-02-10 10:52:54 -0500

    create drizzle migration file

---
+++ drizzle/0000_empty_virginia_dare.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "users" (
+	"userId" serial PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password" text NOT NULL
+);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/singlestore/driver";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { users } from "./schema.js";
 
 const db = drizzle('postgresql://postgres:password@localhost:5432/postgres')
